f3ff97742ae18d60b65475c340dc07b375ff7ab2
kanakogi <kanakogi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-07 10:00:00 +0000

    release 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/check-copy-contentsccc/tags/1.0.1@752851 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/style.js
@@ -0,0 +1,38 @@
+(function($) {
+	
+	$(".theContentWrap-ccc").bind('copy', function() {
+
+		//選択中のテキスト取得
+		var copyText = $.selection();
+		
+		//nonce取得
+		var nonce = $("input#ccc_nonce").val();
+
+		
+		//ajaxで送信
+		$.ajax({
+			type: 'POST',
+			url: CCC.endpoint,
+			data: {
+				'action': CCC.action,
+				'copyText': copyText,
+				'postID': CCC.postID,
+				'url': window.location.href,
+				'remote_addr': CCC.remote_addr,
+				'referrer': document.referrer,
+				'nonce': nonce
+			},
+			success: function(data){
+				if( data.debug == true ){
+					var json_str = JSON.stringify(data);
+					$('#json-data').append(json_str);					
+				}
+			},
+			error: function() {}
+		});
+		
+	});	
+
+
+
+})(jQuery);
